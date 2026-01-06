738b5cafbb95bda58ca18fb4fe0b910696e2ca97
Evan Delagnes <evan.delagnes@gmail.com>
2026-01-06 11:00:58 +0100

    fix: edit description and links

--- __manifest__.py
+++ __manifest__.py
@@ -3,10 +3,17 @@
     'name': "Stock Barcode Force Scan",
     'summary': "Force scanning before manual quantity changes and block unexpected products",
     'description': """
-        This module extends stock_barcode to add security features:
-        - Users cannot modify quantities manually (using +/- buttons) until the product barcode has been scanned
-        - Users cannot add products that are not in the expected move lines
-    """,
+DELETE THIS MODULE WHEN UPGRADING TO ODOO 16 OR HIGHER
+===========================
+
+**Tested and developed for Odoo 15 only**
+
+This module extends stock_barcode to add security features:
+- Users cannot modify quantities manually (using +/- buttons) until the product barcode has been scanned
+- Users cannot add products that are not in the expected move lines
+""",
+    'author': "zloteerer",
+    'website': "https://github.com/zloteerer/odoo-barcode-stock-force-scan",
     'category': 'Inventory/Inventory',
     'version': '15.0.1.0.0',
     'depends': ['stock_barcode'],
